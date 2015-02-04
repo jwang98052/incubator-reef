256dc48a212666dcbf17365ec2743ec41cc9c562
Julia Wang <jwang98052@yahoo.com>
2015-02-03 19:51:30 -0800

    project structure change for utilities

--- lang/cs/Org.Apache.Reef.Utilities/Properties/AssemblyInfo.cs
+++ lang/cs/Org.Apache.Reef.Utilities/Properties/AssemblyInfo.cs
@@ -24,11 +24,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Utilities")]
+[assembly: AssemblyTitle("Org.Apache.Reef.Utilities")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Utilities")]
+[assembly: AssemblyProduct("Org.Apache.Reef.Utilities")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
