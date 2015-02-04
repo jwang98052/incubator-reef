bbfd28d061786cf366ae35d79e2509dc2fe45596
Julia Wang <jwang98052@yahoo.com>
2015-02-04 12:14:08 -0800

    project structure change for Wake and Wake tests

--- lang/cs/Org.Apache.Reef.Wake/Properties/AssemblyInfo.cs
+++ lang/cs/Org.Apache.Reef.Wake/Properties/AssemblyInfo.cs
@@ -24,11 +24,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Wake")]
+[assembly: AssemblyTitle("Org.Apache.Reef.Wake")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Wake")]
+[assembly: AssemblyProduct("Org.Apache.Reef.Wake")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
